d1cea6e5e13a7cf9dc81f2bcb3e729a3f88eefff
Preston Fassbender <fassbenderp0551@gmail.com>
2024-11-14 15:19:10 -0600

    mui radio button

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import { MuiButton } from './components/MuiButton'
+import { MuiRadioButton } from './components/MuiRadioButton'
 import { MuiSelect } from './components/MuiSelect'
 import { MuiTextField } from './components/MuiTextField'
 import { MuiTypography } from './components/MuiTypography'
@@ -14,7 +15,9 @@ function App() {
 
             {/* <MuiTextField /> */}
 
-            <MuiSelect />
+            {/* <MuiSelect /> */}
+
+            <MuiRadioButton />
         </>
     )
 }
