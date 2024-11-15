eef6cd0d6e2b6b8a46f1c05ae150a35596833e1b
Preston Fassbender <fassbenderp0551@gmail.com>
2024-11-15 08:49:32 -0600

    mui stack component

--- src/components/MuiLayout.jsx
+++ src/components/MuiLayout.jsx
@@ -1,29 +1,38 @@
-import { Box } from '@mui/material'
+import { Box, Stack, Divider } from '@mui/material'
 import React from 'react'
 
 export const MuiLayout = () => {
     return (
         <>
-            <Box
-                sx={{
-                    backgroundColor: 'primary.main',
-                    color: 'white',
-                    height: '100px',
-                    width: '300px',
-                    padding: '16px',
-                    '&:hover': {
-                        backgroundColor: 'primary.light'
-                    }
-                }}
+            <Stack
+                sx={{ border: '1px solid red' }}
+                direction={'row'}
+                // direction={'row-reverse'}
+                // direction={'column-reverse'}
+                spacing={2}
+                divider={<Divider orientation='vertical' flexItem/>}
             >
-                Mui Box
-            </Box>
+                <Box
+                    sx={{
+                        backgroundColor: 'primary.main',
+                        color: 'white',
+                        height: '100px',
+                        width: '300px',
+                        padding: '16px',
+                        '&:hover': {
+                            backgroundColor: 'primary.light'
+                        }
+                    }}
+                >
+                    Mui Box
+                </Box>
 
-            <Box component={'span'}>Mui Box as span</Box>
-            
-            <Box display={'flex'} height={'100px'} width={'100px'} bgcolor={'success.light'} p={2}>
-                Mui Box with inline styles
-            </Box>
+                <Box component={'span'} sx={{ backgroundColor: 'warning.light' }}>Mui Box as span</Box>
+
+                <Box display={'flex'} height={'100px'} width={'100px'} bgcolor={'success.light'} p={2}>
+                    Mui Box with inline styles
+                </Box>
+            </Stack>
         </>
     )
 }
